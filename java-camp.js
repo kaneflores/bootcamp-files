99a36a331d95c4a489af796b19cbfd8a52f56bb0
Kane Flores <kaneflores911@gmail.com>
2025-08-23 18:39:14 -0400

    challenge 1

--- java-camp.js
+++ java-camp.js
@@ -1,3 +1,18 @@
-const exarray = [[1,2], 0]
+//Challenge 1
+const myBoolean = true;
+const myString = "hello world";
+const firstNumber = 20;
+let secondNumber = 40;
+secondNumber = 80;
 
-console.log(exarray[0][1])
+let myArray = [myBoolean, myString];
+
+let myObject = {
+    firstProp: myArray,
+    sumProp: firstNumber + secondNumber
+};
+
+console.log(myObject);
+console.log(myObject.sumProp);
+
+console.log(myObject.firstProp[1]);
