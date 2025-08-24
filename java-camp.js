6c933934933e815f6e8490b1208f5f4c9a8a9c51
Kane Flores <kaneflores911@gmail.com>
2025-08-24 12:51:31 -0400

    smallest array challenge

--- java-camp.js
+++ java-camp.js
@@ -111,3 +111,14 @@ function evenOrOdd(number) {
   return "Odd"
   
 }
+
+function findSmallestInt(arr) {
+  let smallest = arr[0]
+  for (let i = 0; i<arr.length; i++){
+    if (arr[i] < smallest){
+      smallest = arr[i];
+    }
+  }
+  return smallest
+
+}
